662e71d039401f4eb78217ffdc911736361519a5
AlexanderGrunewald <grunew14@msu.edu>
2024-01-11 20:50:14 +0100

    added some comments on how the retreiveal method of the pieces works

--- board/board.cpp
+++ board/board.cpp
@@ -1,7 +1,15 @@
 #include <stdint-gcc.h>
 #include <iostream>
 
+/**
+ * @class ChessBoard
+ * @brief Represents a chess board with the current state of the chess pieces
+ *
+ * This class represents a chess board with the current state of the chess pieces.
+ * It provides methods to initialize the board, print the board and retrieve the piece at a specific square.
+ */
 class ChessBoard {
+
 public:
 
     ChessBoard()
@@ -18,10 +26,18 @@ public:
     ~ChessBoard() {
     }
 
+    /**
+     * @brief Prints the current state of the chess board.
+     *
+     * This method prints the current state of the chess board, with the pieces represented
+     * by their corresponding characters. The board is printed with ranks from 8 to 1,
+     * and files from a to h. The black pieces are represented in lowercase and white pieces
+     * in uppercase. An empty square is represented by a dot ('.').
+     */
     void printBoard() {
         for (int rank = 7; rank >= 0; --rank) {
             for (int file = 0; file < 8; ++file) {
-                int square = rank * 8 + file;
+                int square = rank * 8 + file; // finds the index of the square
                 char piece = getPieceAt(square);
                 std::cout << piece << " ";
             }
@@ -51,8 +67,8 @@ private:
     U64 allPieces;
 
     char getPieceAt(int square) const {
-        U64 mask = 1ULL << square;
-        if (whitePawns & mask) return 'P';
+        U64 mask = 1ULL << square; // shifts the 1 to the square we are looking for
+        if (whitePawns & mask) return 'P'; // the code below well then see if there is a piece on the square
         if (whiteKnights & mask) return 'N';
         if (whiteBishops & mask) return 'B';
         if (whiteRooks & mask) return 'R';
