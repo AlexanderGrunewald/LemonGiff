30ad08f7bc1ced0e6bdfd801e2098796440bc945
AlexanderGrunewald <grunew14@msu.edu>
2024-01-11 21:27:57 +0100

    adding a file for the piece rules

---
+++ board/chessRules.cpp
@@ -0,0 +1,7 @@
+//
+// Created by alexg on 1/11/2024.
+//
+
+#include "chessRules.h"
+
+
---
+++ board/chessRules.h
@@ -0,0 +1,15 @@
+//
+// Created by alexg on 1/11/2024.
+//
+
+#ifndef PERSONALPROJECTS_CHESSRULES_H
+#define PERSONALPROJECTS_CHESSRULES_H
+
+
+class chessRules {
+public: chessRules();
+
+};
+
+
+#endif //PERSONALPROJECTS_CHESSRULES_H
