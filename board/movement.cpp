b6dc57bad88f41f58ffd98ba2c81ec4f00f3335b
AlexanderGrunewald <grunew14@msu.edu>
2024-01-12 13:53:13 +0100

    implemented functions that allow a piece to move any of the 8 directions

--- board/movement.cpp
+++ board/movement.cpp
@@ -3,3 +3,31 @@
 U64 Movement::moveNorth(U64 piece, int times) {
     return piece << 8 *times;
 }
+
+U64 Movement::moveEast(U64 piece, int times) {
+    return piece >> 1 * times;
+}
+
+U64 Movement::moveWest(U64 piece, int times) {
+    return piece << 1 * times;
+}
+
+U64 Movement::moveSouth(U64 piece, int times) {
+    return piece >> 8 * times;
+}
+
+U64 Movement::moveNorthEast(U64 piece, int times) {
+    return piece >> 7 * times;
+}
+
+U64 Movement::moveNorthWest(U64 piece, int times) {
+    return piece >> 9 * times;
+}
+
+U64 Movement::moveSouthEast(U64 piece, int times) {
+    return piece >> 9 * times;
+}
+
+U64 Movement::moveSouthWest(U64 piece, int times) {
+    return piece >> 7 * times;
+}
--- board/movement.h
+++ board/movement.h
@@ -6,13 +6,13 @@ typedef uint64_t U64;
 class Movement {
 public:
     static U64 moveNorth(U64 piece, int times);
-    static U64 moveSouth(U64 piece);
-    static U64 moveWest(U64 piece);
-    static U64 moveEast(U64 piece);
-    static U64 moveNorthEast(U64 piece);
-    static U64 moveNorthWest(U64 piece);
-    static U64 moveSouthEast(U64 piece);
-    static U64 moveSouthWest(U64 piece);
+    static U64 moveSouth(U64 piece, int times);
+    static U64 moveWest(U64 piece, int times);
+    static U64 moveEast(U64 piece, int times);
+    static U64 moveNorthEast(U64 piece, int times);
+    static U64 moveNorthWest(U64 piece, int times);
+    static U64 moveSouthEast(U64 piece, int times);
+    static U64 moveSouthWest(U64 piece, int times);
 };
 
 #endif
